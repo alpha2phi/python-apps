dd8bac674419d368a4fa74a95ab64aa236827442
Koh Meng Wang <mengwangk@gmail.com>
2020-12-27 14:32:27 +0800

    Initial commit

--- gan-model-serving/frontend/main.py
+++ gan-model-serving/frontend/main.py
@@ -1,4 +1,6 @@
+import requests
 import streamlit as st
+from streamlit import uploaded_file_manager
 from streamlit.cli import main
 
 st.title("GAN Model Serving Sample")
@@ -7,11 +9,21 @@ st.write(
     """Serving a GAN model using FastAPI and Streamlit."""
 )  
 
-input_image = st.file_uploader("Upload Image")
+uploaded_image = st.file_uploader("Upload Image")
 
 if st.button("Process"):
-    col1, col2 = st.beta_columns(2)
-    if input_image:
-        pass
+    # col1, col2 = st.beta_columns(2)
+    if uploaded_image is not None:
+        # File details
+        file_details = {"FileName": uploaded_image.name,"FileType": uploaded_image.type,"FileSize":uploaded_image.size}
+        st.write(file_details)
+
+        # File content
+        file_content = {"file": uploaded_image.getvalue()}
+        st.write(file_content)
+        # res = requests.post(f"http://backend:8080/{style}", files=files)
+        # img_path = res.json()
+        # image = Image.open(img_path.get("name"))
+        # st.image(image)
     else:
         st.write("Upload image!")
