e903e332c870833fe1b22176ab854d46411d5ce2
Koh Meng Wang <mengwangk@gmail.com>
2020-12-27 15:33:17 +0800

    Initial commit

--- gan-model-serving/backend/main.py
+++ gan-model-serving/backend/main.py
@@ -1,17 +1,16 @@
 import uvicorn
+from fastapi import FastAPI, File
 from starlette.responses import Response
 
-from fastapi import FastAPI, File
-
 app = FastAPI(
-    title="GAN Model Serving",
+    title="GAN Model Serving Tutorial",
     description="""Serving a GAN model. Visit port 8501 for the Streamlit interface.""",
     version="0.0.1"
 )
 
 @app.get("/")
 def home():
-    return {"message": "A GAN Model Serving Sample"}
+    return {"message": "A GAN Model Serving Tutorial"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8088)
+    uvicorn.run("main:app", host="0.0.0.0", port=8088, reload=True)
