e431d566d99bb9efd588d47b57812236c98ddaf4
Koh Meng Wang <mengwangk@gmail.com>
2020-12-27 23:48:34 +0800

    Initial commit

--- gan-model-serving/backend/main.py
+++ gan-model-serving/backend/main.py
@@ -1,5 +1,10 @@
+import asyncio
+import io
+import uuid
+
 import uvicorn
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
 from starlette.responses import Response
 
 app = FastAPI(
@@ -11,6 +16,18 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {"message": "A GAN Model Serving Tutorial"}
+
+@app.post("/process")
+def process(file: UploadFile = File(...)):
+    file_bytes = file.file.read()
+    image = Image.open(io.BytesIO(file_bytes))
+    name = f"/data/{str(uuid.uuid4())}.jpg"
+    image.save(name) 
+    return Response(file_bytes, media_type="image/jpg")
+
+@app.get("/generate")
+def generate():
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8088, reload=True)
--- gan-model-serving/frontend/main.py
+++ gan-model-serving/frontend/main.py
@@ -1,9 +1,12 @@
+import io
+
 import requests
 import streamlit as st
+from PIL import Image
 from streamlit import uploaded_file_manager
 from streamlit.cli import main
 
-server_url=f"http://localhost:8088/{process}"
+server_url=f"http://backend:8088/process"
 
 st.title("GAN Model Serving Sample")
 st.write(
@@ -12,19 +15,26 @@ st.write(
 uploaded_image = st.file_uploader("Upload Image")
 
 if st.button("Process"):
-    # col1, col2 = st.beta_columns(2)
     if uploaded_image is not None:
         # File details
         file_details = {"FileName": uploaded_image.name,"FileType": uploaded_image.type,"FileSize":uploaded_image.size}
         st.write(file_details)
 
         # File content
-        file_content = {"file": uploaded_image.getvalue()}
-        # st.write(file_content)
+        files = {"file": uploaded_image.getvalue()}
 
-        # res = requests.post(f"http://backend:8080/{style}", files=files)
-        # img_path = res.json()
-        # image = Image.open(img_path.get("name"))
-        # st.image(image)
+        col1, col2 = st.beta_columns(2)
+
+        # Show original image
+        original_image = Image.open(uploaded_image)
+        col1.header("Original Image")
+        col1.image(original_image, use_column_width=True)
+
+        # Post to server
+        res = requests.post(server_url, files=files)
+        processed_image = Image.open(io.BytesIO(res.content))
+        col2.header("Processed Image")
+        col2.image(processed_image, use_column_width=True)
+
     else:
         st.write("Upload image!")
