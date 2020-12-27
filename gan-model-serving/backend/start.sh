f321c3318cc73e9b43e3f04ca004b13b8dde994e
Koh Meng Wang <mengwangk@gmail.com>
2020-12-27 21:26:30 +0800

    Initial commit

--- gan-model-serving/backend/start.sh
+++ gan-model-serving/backend/start.sh
@@ -3,8 +3,9 @@ set -e
 
 if [ "$DEBUG" = true ] ; then
     echo 'Debugging - ON'
-    nodemon --exec streamlit run main.py
+    uvicorn main:app --host 0.0.0.0 --port 8088 --reload
 else
     echo 'Debugging - OFF'
-    streamlit run main.py
+    uvicorn main:app --host 0.0.0.0 --port 8088 
 fi
+
