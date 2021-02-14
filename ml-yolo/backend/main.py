6ad9eb4aaa2b6e04ff70719968f56ac3a2ef8c17
Meng Wang <mengwangk@gmail.com>
2021-02-15 01:02:32 +0800

    Initial commit

--- ml-yolo/backend/main.py
+++ ml-yolo/backend/main.py
@@ -1,12 +1,17 @@
 import io
 import json
 import uuid
-
+import logging
+import sys
 import uvicorn
 from PIL import Image
 from starlette.responses import Response
-
 from fastapi import FastAPI, File, UploadFile
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+from model import yolov5
+
 
 app = FastAPI(
     title="Serving YOLO",
@@ -18,6 +23,16 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {"message": "YOLO - You Only Look Once"}
+
+
+@app.get("/yolov5")
+def process_yolov5(file: UploadFile = File(...)):
+    file_bytes = file.file.read()
+    image = Image.open(io.BytesIO(file_bytes))
+    name = f"/data/{str(uuid.uuid4())}.jpg"
+    image.save(name)
+    classes, converted_img = yolov5(image)
+    return converted_img
 
 
 if __name__ == "__main__":
---
+++ ml-yolo/backend/model/yolov5.py
@@ -0,0 +1,33 @@
+import torch
+import os
+import logging
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+yolo_model = os.getenv("YOLO_MODEL", "yolov5s")
+
+logging.info(f"YOLO model - {yolo_model}")
+
+model = torch.hub.load("ultralytics/yolov5", yolo_model, pretrained=True)
+
+
+def yolov5(img):
+    """Process a PIL image."""
+
+    # Inference
+    results = model(img)
+
+    detected_classes = ""
+    if results.pred is not None:
+        pred = results.pred[0]
+        if pred is not None:
+            for c in pred[:, -1].unique():
+                n = (pred[:, -1] == c).sum()
+                detected_classes += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+
+    logging.DEBUG(f"Detected classes: {detected_classes}")
+
+    rendered_imgs = results.render()
+    converted_img = Image.fromarray(rendered_imgs[0]).convert("RGB")
+
+    return detected_classes, converted_img
