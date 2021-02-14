e3c0f8b92a8cf75e6c1602b8cf80bf73061ba94f
Meng Wang <mengwangk@gmail.com>
2021-02-14 11:13:33 +0800

    Initial commit

--- ml-yolo/backend/tests/test_yolo_model.py
+++ ml-yolo/backend/tests/test_yolo_model.py
@@ -2,6 +2,7 @@ import unittest
 
 import torch
 import torchvision
+from PIL import Image
 
 print(torch.__version__)
 
@@ -9,7 +10,6 @@ model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
 
 
 class TestYoloModel(unittest.TestCase):
-
     """Test YOLO model."""
 
     def setUp(self):
@@ -20,14 +20,20 @@ class TestYoloModel(unittest.TestCase):
 
     def test_model(self):
         # Images
-        imgs = ["backend/test_images/bus.jpg"]  # batched list of images
+        imgs = [
+            "backend/test_images/bus.jpg",
+            "backend/test_images/zidane.jpg",
+            "backend/test_images/bear.jpg",
+            "backend/test_images/dragon.jpg",
+        ]
+        img = Image.open(imgs[3])
 
         # Inference
-        results = model(imgs)
+        results = model(img)
+        self.assertIsNotNone(results)
 
         # Results
         results.print()
-        results.save()  # or .show()
-
-        # Data
+        results.save()
+        # results.show()
         print(results.xyxy[0])  # print img1 predictions (pixels)
