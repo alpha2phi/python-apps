5bd7ebeeca59197592a66f3fe6fac66634756103
Meng Wang <mengwangk@gmail.com>
2021-02-15 12:31:14 +0800

    Initial commit

--- ml-yolo/backend/main.py
+++ ml-yolo/backend/main.py
@@ -4,15 +4,18 @@ import uuid
 import logging
 import sys
 import uvicorn
+from typing import List
 from PIL import Image
 from starlette.responses import Response
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 from model import yolov5
 
 
+# FastAPI
 app = FastAPI(
     title="Serving YOLO",
     description="""Visit port 8088/docs for the FastAPI documentation.""",
@@ -20,12 +23,36 @@ app = FastAPI(
 )
 
 
+class ConnectionManager:
+    """Web socket connection manager."""
+
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+conn_mgr = ConnectionManager()
+
+
 @app.get("/")
 def home():
     return {"message": "YOLO - You Only Look Once"}
 
 
-@app.post("/yolov5")
+@app.post("/yolo")
 def process_yolov5(file: UploadFile = File(...)):
     file_bytes = file.file.read()
     image = Image.open(io.BytesIO(file_bytes))
@@ -39,6 +66,62 @@ def process_yolov5(file: UploadFile = File(...)):
     converted_img.save(name)
     converted_img.save(bytes_io, format="PNG")
     return Response(bytes_io.getvalue(), media_type="image/png")
+
+
+@app.websocket("/yolo_ws/{client_id}")
+async def process_yolov5_ws(websocket: WebSocket, client_id: int):
+    await conn_mgr.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await conn_mgr.send_message(f"You wrote: {data}", websocket)
+            # await conn_mgr.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        conn_mgr.disconnect(websocket)
+        await conn_mgr.broadcast(f"Client #{client_id} left the chat")
+
+
+ws_client_html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8088/yolo_ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/yolo_client")
+async def process_yolo_client():
+    return HTMLResponse(ws_client_html)
 
 
 if __name__ == "__main__":
