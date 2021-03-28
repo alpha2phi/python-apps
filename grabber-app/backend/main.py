d1bcd6a0adafd58a6445fec7c25fcdab90cab26a
Meng Wang <mengwangk@gmail.com>
2021-03-28 15:20:50 +0800

    Initial commit

--- grabber-app/backend/main.py
+++ grabber-app/backend/main.py
@@ -1,9 +1,19 @@
 import sys
 import logging
+import graphene
 from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
 import uvicorn
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return "Hello " + name
+
 
 app = FastAPI(
     title="Grabber App",
@@ -11,10 +21,7 @@ app = FastAPI(
     version="0.0.1",
 )
 
-
-@app.get("/")
-def home():
-    return {"message": "Cartoon Camera"}
+app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8088, reload=True)
