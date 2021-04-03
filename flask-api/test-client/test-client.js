dc52e68b8ab9066cb8917ccba974b00c30d69eda
Meng Wang <mengwangk@gmail.com>
2021-04-03 22:49:03 +0800

    Update test client

--- flask-api/test-client/test-client.js
+++ flask-api/test-client/test-client.js
@@ -1,3 +1,22 @@
+// Insert faker accounts
+const faker = require("faker");
+const axios = require('axios');
+
+for (i = 0; i < 15; i++) {
+    axios.post('http://localhost:5000/api/v1/accounts', {
+        username: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Search for alpha2phi
 var filter = {};
 filter.col = "email";
 filter.opr = "eq";
@@ -10,7 +29,6 @@ filters_criteria = JSON.stringify({"filters" : filters});
 console.log(encodeURI(filters_criteria));
 
 const query = "http://localhost:5000/api/v1/accounts/?q=" + encodeURI(filters_criteria)
-const axios = require('axios');
 axios.get(query)
   .then(response => {
     console.log(response.data);
