0f95ce7882b9d5202eb0ce7ef0371c7b9d544588
Meng Wang <mengwangk@gmail.com>
2021-04-03 11:02:23 +0800

    Initial commit

---
+++ flask-api/app/views.py
@@ -0,0 +1,50 @@
+from flask import render_template
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import ModelView, ModelRestApi
+
+from . import appbuilder, db
+
+"""
+    Create your Model based REST API::
+
+    class MyModelApi(ModelRestApi):
+        datamodel = SQLAInterface(MyModel)
+
+    appbuilder.add_api(MyModelApi)
+
+
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(
+        MyModelView,
+        "My View",
+        icon="fa-folder-open-o",
+        category="My Category",
+        category_icon='fa-envelope'
+    )
+"""
+
+"""
+    Application wide 404 error handler
+"""
+
+
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return (
+        render_template(
+            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+        ),
+        404,
+    )
+
+
+db.create_all()
