f95cfcf5e40aa04fd214f8eeb4fffd0732157a5e
Meng Wang <mengwangk@gmail.com>
2021-04-04 14:44:01 +0800

    Update with frontend

--- hackernews/frontend-react/src/index.js
+++ hackernews/frontend-react/src/index.js
@@ -1,14 +1,32 @@
-import './styles/index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000'
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
