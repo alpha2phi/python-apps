a992cfcf8c08dd996a9a796269197288e947fe47
Meng Wang <mengwangk@gmail.com>
2021-04-18 15:06:27 +0800

    Initial commit

--- serving-ml-models/backend/tests/test_models.py
+++ serving-ml-models/backend/tests/test_models.py
@@ -1,16 +1,19 @@
 import unittest
-from ppgan.apps import Photo2CartoonPredictor
 import cv2
+from ppgan.apps import Photo2CartoonPredictor
+from retinaface_cov import RetinaFaceCoV
 
 
 class TestModels(unittest.TestCase):
+    """Model test cases."""
+    @unittest.skip("Tested successful!")
     def test_photo2cartoon(self):
         p2c = Photo2CartoonPredictor()
         nd_arr = p2c.run('test_images/photo3.jpg')
         image = cv2.cvtColor(nd_arr, cv2.COLOR_RGB2BGR)
         print(type(image))
 
-    def test_retinaface_anti_cov_face_detector():
+    def test_retinaface_anti_cov_face_detector(self):
         pass
 
 
