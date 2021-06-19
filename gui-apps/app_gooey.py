8325fc4c072289ba8919480db519e403ec210da7
Meng Wang <mengwangk@gmail.com>
2021-06-19 23:46:50 +0800

    Initial commit

---
+++ gui-apps/app_gooey.py
@@ -0,0 +1,27 @@
+'''
+A simple Gooey example. One required field, one optional.
+'''
+
+from gooey import Gooey, GooeyParser
+
+
+@Gooey()
+def main():
+    parser = GooeyParser(description='Process some integers.')
+
+    parser.add_argument('required_field',
+                        metavar='Some Field',
+                        help='Enter some text!')
+
+    parser.add_argument('-f',
+                        '--foo',
+                        metavar='Some Flag',
+                        action='store_true',
+                        help='I turn things on and off')
+
+    parser.parse_args()
+    print('Hooray!')
+
+
+if __name__ == '__main__':
+    main()
