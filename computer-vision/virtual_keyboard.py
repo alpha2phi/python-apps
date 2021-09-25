043e30bb865180aab006e0b23c6a15acaf30967a
alpha2phi <alpha2phi@gmail.com>
2021-09-25 10:56:11 +0800

    Initial commit

--- computer-vision/virtual_keyboard.py
+++ computer-vision/virtual_keyboard.py
@@ -1,8 +1,10 @@
+from time import sleep
+
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
-from time import sleep
 import numpy as np
+
 from pynput.keyboard import Controller
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
